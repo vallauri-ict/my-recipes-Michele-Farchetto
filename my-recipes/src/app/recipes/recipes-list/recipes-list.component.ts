56c2a328c1204030bf0819df2b32bcb060da1393
Michele-Farchetto <71264401+Michele-Farchetto@users.noreply.github.com>
2020-12-22 13:20:18 +0100

    insert recipes and managed header and recipe-list

--- my-recipes/src/app/recipes/recipes-list/recipes-list.component.ts
+++ my-recipes/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Carbonara','Tipica Piatto di pasta italiana della città di Roma','https://wips.plug.it/cips/buonissimo.org/cms/2020/04/spaghetti-carbonara.jpg')
+  ];
 
   constructor() { }
 
