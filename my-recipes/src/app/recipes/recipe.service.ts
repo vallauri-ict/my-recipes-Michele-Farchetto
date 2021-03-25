de21b557617efc0ce04252c370449e0d77b2dfca
Michele-Farchetto <71264401+Michele-Farchetto@users.noreply.github.com>
2021-03-25 08:57:04 +0100

    Lezione 25/03/2021 - Ingredient Services

--- my-recipes/src/app/recipes/recipe.service.ts
+++ my-recipes/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,9 +7,33 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Carbonara', 'Tipica pasta con pancetta ecc..', 'https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2018/01/1517219140_f055c0757a72a3a48b267d0878cf5344e3a3f6b1-1517074283.png?w=580'),
-        new Recipe('Amatriciana', 'Un primo piatto della tradizione laziale...', 'https://www.buttalapasta.it/wp-content/uploads/sites/3/2020/04/bucatini-amatriciana.jpg'),
-        new Recipe('Lasagne', 'In Italia questo piatto assume diverse declinazioni...', 'https://www.ricette.com/wp-content/uploads/2013/12/lasagne-alla-bolognese.jpg')
+        new Recipe('Carbonara', 
+        'Tipica pasta con pancetta ecc..', 
+        'https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2018/01/1517219140_f055c0757a72a3a48b267d0878cf5344e3a3f6b1-1517074283.png?w=580',
+        [
+            new Ingredient('Pepe',50),
+            new Ingredient('Pecorino',150),
+            new Ingredient('Guanciale',80),
+            new Ingredient('Pasta',200)
+        ]),
+        new Recipe('Amatriciana', 
+        'Un primo piatto della tradizione laziale...', 
+        'https://www.buttalapasta.it/wp-content/uploads/sites/3/2020/04/bucatini-amatriciana.jpg',
+        [
+            new Ingredient('Passata di pomodoro',200),
+            new Ingredient('Guanciale',80),
+            new Ingredient('Pasta',200),
+            new Ingredient('Pecorino',80)
+        ]),
+        new Recipe('Lasagne', 
+        'In Italia questo piatto assume diverse declinazioni...', 
+        'https://www.ricette.com/wp-content/uploads/2013/12/lasagne-alla-bolognese.jpg',
+        [
+            new Ingredient('Ragu',100),
+            new Ingredient('Romagnole',150),
+            new Ingredient('Besciamella',80),
+            new Ingredient('Parmigiano',50)
+        ])
     ];
 
     getRecipes() {
--- my-recipes/src/app/shopping-list/shopping-list.service.ts
+++ my-recipes/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
-export class ShoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
+export class ShoppingListService {
+    
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Pancetta', 50),
+        new Ingredient('Pasta', 750)
+    ];
+
+    getIngredients(){
+        return this.ingredients.slice();//si passa una copia non un riferiemetno
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
